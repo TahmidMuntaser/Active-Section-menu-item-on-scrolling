dbfe70b6ce7d61ac82a5f024523d28d7ef03b6e4
Tahmid Muntaser <115565668+TahmidMuntaser@users.noreply.github.com>
2024-02-24 02:14:59 +0600

    update 4.2

--- main.js
+++ main.js
@@ -40,3 +40,15 @@ window.addEventListener("scroll", ()=>{
         
     })
 });
+
+
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", ()=>{
+    if(window.pageYOffset > 100){
+        toTop.classList.add('active');   
+    }
+    else{
+        toTop.classList.remove('active');
+    }
+});
